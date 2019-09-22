507a2499dca6727e46487fa56dfe4ac8205d2f4d
Michael Nunes <miketnunes@gmail.com>
2019-09-22 09:07:26 -0700

    Add options UI page
    Shows an input with isEnabled state as an example

---
+++ extension/options/options.js
@@ -0,0 +1,11 @@
+function getOptions() {
+
+  const setInput = data => document.querySelector("#isEnabled").value = data.isEnabled;
+
+  const onError = err => console.log(`Error: ${err}`);
+
+  let data = browser.storage.local.get('isEnabled');
+  data.then(setInput, onError);
+}
+
+document.addEventListener("DOMContentLoaded", getOptions);
