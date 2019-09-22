974aa88b7d06dea69111c1001b064cf51715d06d
Michael Nunes <miketnunes@gmail.com>
2019-09-21 21:26:56 -0700

    Enable page action only on Lambda Training Kit

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,6 @@
 const CSS = "body { border: 20px solid red; }";
 const TITLE_APPLY = "Apply CSS";
 const TITLE_REMOVE = "Remove CSS";
-const APPLICABLE_PROTOCOLS = ["http:", "https:"];
 
 /*
 Toggle CSS: based on the current title, insert or remove the CSS.
@@ -26,41 +25,32 @@ function toggleCSS(tab) {
 }
 
 /*
-Returns true only if the URL's protocol is in APPLICABLE_PROTOCOLS.
-*/
-function protocolIsApplicable(url) {
-  var anchor = document.createElement('a');
-  anchor.href = url;
-  return APPLICABLE_PROTOCOLS.includes(anchor.protocol);
-}
-
-/*
 Initialize the page action: set icon and title, then show.
 Only operates on tabs whose URL's protocol is applicable.
 */
 function initializePageAction(tab) {
-  if (protocolIsApplicable(tab.url)) {
-    browser.pageAction.setIcon({ tabId: tab.id, path: "icons/off.svg" });
-    browser.pageAction.setTitle({ tabId: tab.id, title: TITLE_APPLY });
-    browser.pageAction.show(tab.id);
-  }
+  browser.pageAction.setIcon({ tabId: tab.id, path: "icons/off.svg" });
+  browser.pageAction.setTitle({ tabId: tab.id, title: TITLE_APPLY });
+  browser.pageAction.show(tab.id);
 }
 
 /*
-When first loaded, initialize the page action for all tabs.
+When first loaded, initialize the page action for all tabs matching our url query.
 */
-var gettingAllTabs = browser.tabs.query({});
-gettingAllTabs.then((tabs) => {
+var matchingTabs = browser.tabs.query({ url: "*://learn.lambdaschool.com/*" });
+matchingTabs.then((tabs) => {
   for (let tab of tabs) {
     initializePageAction(tab);
   }
 });
 
 /*
-Each time a tab is updated, reset the page action for that tab.
+Each time a tab is updated, reset the page action for that tab only if it's url still matches learn.lambdaschool.com.
 */
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  initializePageAction(tab);
+  if (tab.url.search("learn.lambdaschool.com") !== -1) {
+    initializePageAction(tab);
+  }
 });
 
 /*
